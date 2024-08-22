ea5f1197aef4d471cb6b46e07cb99fc39d58c444
Matthew Pitropov <matthewpitropov@gmail.com>
2024-08-21 22:44:54 -0400

    Updating bash scripts and notes

---
+++ exec.bash
@@ -0,0 +1,1 @@
+docker exec -it robotdocker_container /bin/bash
--- run.bash
+++ run.bash
@@ -1,8 +1,8 @@
 xhost +local:docker
 
-docker run -it --net=host \
+docker run --name robotdocker_container -it --net=host \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="/home/wavelab/git/robot/packages:/home/packages:rw" \
+  --volume="/home/matthew/Projects/robot/packages:/home/packages:rw" \
   robotdocker bash
